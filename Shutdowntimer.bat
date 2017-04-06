3f1dee84c3934d43a785abc4999a65a6801a3840
L4ma <m.damm@mnet-online.de>
2017-04-06 14:48:32 +0200

    shutdowntimer v 1.1
    changes:
    languagesettings

--- Shutdowntimer.bat
+++ Shutdowntimer.bat
@@ -1,9 +1,11 @@
 @echo off
 color 0A
-
+echo #####################
+echo #   shutdowntimer   #
+echo #####################
 echo How long?
-set /p min=Time in Minutes:
+set /p min=time in minutes:
 set /a zeit = %min%*60
-echo shutdown in %min% Minutes
+echo shutdown in %min% minutes
 pause
 shutdown.exe -s -t %zeit% -f
