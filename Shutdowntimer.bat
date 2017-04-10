d1476076e1a3bfb0cd4da85e15dbc9506303f38b
L4ma <m.damm@mnet-online.de>
2017-04-10 07:45:32 +0200

    shutdowntimer v 1.2
    little changes

--- Shutdowntimer.bat
+++ Shutdowntimer.bat
@@ -1,11 +1,21 @@
 @echo off
 color 0A
+
+title shutdowntimer by L4ma
 echo #####################
+
 echo #   shutdowntimer   #
+
 echo #####################
+
 echo How long?
+
 set /p min=time in minutes:
+
 set /a zeit = %min%*60
+
 echo shutdown in %min% minutes
+
 pause
+
 shutdown.exe -s -t %zeit% -f
